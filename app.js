2d5449368f525ccb69f5d399eee1520fe2107544
Cheni Chen <celiachenic@gmail.com>
2021-05-21 21:27:48 +0800

    六層星星樹

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let div = document.querySelector("div")
+
+
+for(let i = 0; i< 6; i++){
+    document.write("<br>")
+    for(let j = 0; j<= i ; j++){
+        document.write("<span>*</span>")
+    }
+}
